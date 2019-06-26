20d23c020ac4c2801efbe9e7261f2d11681e2640
Hamza Sheikh <hs@aikchar.dev>
2019-06-26 14:59:58 -0700

    Updates & fixes after two years
    Things have changed and so must this repo.

--- README.md
+++ README.md
@@ -2,63 +2,109 @@
 
 An Azure Service Principal is required for applications like Terraform to authenticate.
 
-This is a quick start guide on bootstrapping Service Principal in Azure. All steps are done
-using *tasks.py* with the [Invoke](http://docs.pyinvoke.org/en/latest/) library. It serves as both the admin interface
-as well as documentation on how those steps are performed.
+This is a quick start guide on bootstrapping Service Principal in Azure. All steps are done using *tasks.py* with
+the [Invoke](http://docs.pyinvoke.org/en/latest/) library. It serves as both the admin interface as well as
+documentation on how those steps are performed.
 
 # Prerequities
 
-Obtain login to Azure with owner permissions for at least one subscription. It
-will be used to create a Service Principal that applications (like Terraform) will use 
-to authenticate to Azure.
+Obtain login to Azure with owner permissions for at least one subscription. It will be used to create a Service
+Principal that applications (like Terraform) will use to authenticate to Azure.
 
 Install:
 
-* Python (3.6+ recommended)
+* Python (3.7+ recommended)
 * pipenv
 * Docker
+* [direnv](https://github.com/direnv/direnv)
 
 # pipenv
 
-After cloning this repo, cd into this directory and use ``pipenv`` to install prereq Python packages.
+After cloning this repo, cd into this directory and use ``pipenv`` to install required Python packages.
 
-        $ pipenv install
+    $ pipenv install
 
 # Bootstrap
 
-Export these environment variables before running ``make``. If any one of them is not set, ``make``
-will fail.
+Export these environment variables before running ``invoke``. If any one of them is not set, ``invoke`` will fail.
 
-* AZURE_CLI_VERSION - Which version of Azure CLI 2.0 to use. This guide was written with *2.0.20*.
 * AZURE_LOGIN_USER - Login user name for Azure CLI (same as Azure Portal)
 * AZURE_LOGIN_PASSWD - Login password for Azure CLI
 * AZURE_LOCATION - Name of location in Azure to stand up the environment
 * AZURE_AD_APP_NAME - Name of the Active Directory (AD) app to create in Azure (any unique name)
-* AZURE_AD_APP_PASSWORD - Come up with a password for the AD app to create in Azure
 * AZURE_SUBSCRIPTION_NAME - Name of the Azure subscription to use (look it up on Azure Portal)
 
-For your convenience, the file *environment_variables* contains dummy values for these variables but
-they're commented out. Change the values to suit your needs, uncomment the lines as needed, and 
-source the file.
+For your convenience, the file *.envrc* contains empty values for these variables. Add the values to suit your needs
+and source the file.
 
-        $ #edit environment_variables
-        $ source ./environment_variables
+    $ direnv allow .
+    $ #edit .envrc
+    $ direnv reload
 
 Create Service Principal and Application in Azure.
 
-        $ pipenv shell
-        $ inv bootstrap
-        $ exit
+    $ pipenv shell
+    $ invoke containerup
+    $ invoke bootstrap
+    $ exit
 
-Once the bootstrap is complete, it will create a *cache.json* file in the current directory.
-**Keep this information safe because it may contain sensitive information.**
+Once the bootstrap is complete, it will:
+
+- Create a Service Principal with _Reader_ role. To override the default role, run it as
+``invoke bootstrap --role Contributor``, for example.
+- Create a *cache.json* file in the current directory. **Keep this information safe.**
 
 # Teardown
 
-TODO
+Get the Azure AD App ID,
+
+    $ az role assignment list --assignee http://"${AZURE_AD_APP_NAME}"
+
+Delete the service principal using the _principalId_ from above step,
+
+    $ az ad sp delete --id PRINCIPALID_FROM_ABOVE
 
 # Troubleshooting
 
-*tasks.py* can run into numerous errors. The first place to get an idea of what may
-have gone wrong is to read the error and *inv.log* file. Then read *tasks.py* to understand the steps performed
-before the error occurred.
+*tasks.py* can run into numerous errors. The first place to get an idea of what may have gone wrong is to read the
+error on stdout and *invoke.log*. Then read *tasks.py* to understand the steps performed before the error occurred.
+
+# Interactive Setup
+
+Read official documentation first: [Create an Azure service principal with Azure CLI](https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli?view=azure-cli-latest)
+
+Use interactive login and follow instructions. You'll be asked to enter a code on some dynamically generated link.
+
+    $ az login
+
+The output of the interactive login process will contain some important information.
+
+* _name_ is the subscription name
+* _id_ is the subscription ID
+* _tenantId_ is the tenant ID
+
+    $ export AZURE_SUBSCRIPTION_NAME
+    $ AZURE_SUBSCRIPTION_NAME=name_FROM_OUTPUT_OF_PREVIOUS_STEP
+
+    $ export AZURE_SUBSCRIPTION_ID
+    $ AZURE_SUBSCRIPTION_ID=id_FROM_OUTPUT_OF_PREVIOUS_STEP
+
+    $ export AZURE_LOCATION
+    $ AZURE_LOCATION='eastus2'
+
+    $ export AZURE_AD_APP_NAME
+    $ AZURE_AD_APP_NAME=UNIQUE_NAME_OF_YOUR_CHOICE
+
+    $ export AZURE_TENANT_ID
+    $ AZURE_TENANT_ID=tenantId_FROM_OUTPUT_OF_PREVIOUS_STEP
+
+Create Service Principal. If you're a *Contributor* yourself, you cannot run this command successfully. An *Owner*
+needs to do it for you.
+
+    $ az ad sp create-for-rbac --role='Reader/or/Contributor' --name="${AZURE_AD_APP_NAME}"
+
+The output from above contains _appId_, which is to be exported as an environment variable. This _appId_ is the ID of
+the newly created Service Principal.
+
+    $ export AZURE_AD_SP_ID
+    $ AZURE_AD_SP_ID=appId_FROM_OUTPUT_OF_PREVIOUS_STEP
--- tasks.py
+++ tasks.py
@@ -1,22 +1,26 @@
 import copy
 import json
+import logging
 import os
 
 import daiquiri
 from invoke import task
 from invoke.exceptions import UnexpectedExit, Failure
 
-daiquiri.setup(outputs=(
-    daiquiri.output.STDERR,
-    # daiquiri.output.Syslog(),
-    daiquiri.output.File(directory="."),
-))
+daiquiri.setup(
+    level=logging.DEBUG,
+    outputs=(
+        daiquiri.output.STDERR,
+        # daiquiri.output.Syslog(),
+        daiquiri.output.File(directory="."),
+    ),
+)
 logger = daiquiri.getLogger()
 
 docker_compose_project_name = "azurespbootstrap"
 docker_compose_azurecli_service = "azurecli"
 docker_compose_cli = f"docker-compose --project-name {docker_compose_project_name}"
-az_cli = f"{docker_compose_cli} exec {docker_compose_azurecli_service} /usr/local/bin/az"
+az_cli = f"{docker_compose_cli} exec -T {docker_compose_azurecli_service} /usr/local/bin/az"
 cache_file = os.path.join(".", "cache.json")
 
 
@@ -30,13 +34,13 @@ def prerequisites(ctx):
             logger.error(f"Command not found -> {cli}")
             raise e
 
-    env_vars = ["AZURE_CLI_VERSION",
-                "AZURE_LOGIN_USER",
-                "AZURE_LOGIN_PASSWORD",
-                "AZURE_LOCATION",
-                "AZURE_AD_APP_NAME",
-                "AZURE_AD_APP_PASSWORD",
-                "AZURE_SUBSCRIPTION_NAME"]
+    env_vars = [
+        "AZURE_LOGIN_USER",
+        "AZURE_LOGIN_PASSWORD",
+        "AZURE_LOCATION",
+        "AZURE_AD_APP_NAME",
+        "AZURE_SUBSCRIPTION_NAME",
+    ]
 
     errors = []
     for var in env_vars:
@@ -64,25 +68,31 @@ def containerup(ctx):
     cmd = f"{docker_compose_cli} up -d {docker_compose_azurecli_service}"
     logger.debug(f"cmd -> {cmd}")
     result1 = ctx.run(cmd, hide=True)
+    print(result1.return_code)
 
     user = os.environ["AZURE_LOGIN_USER"]
     password = os.environ["AZURE_LOGIN_PASSWORD"]
 
     cmd = f"{az_cli} login -u '{user}' -p '{password}'"
-    logger.debug(f"cmd -> {cmd}".replace(password, '*****'))
+    logger.debug(f"cmd -> {cmd}".replace(password, "*****"))
     result2 = ctx.run(cmd, hide=True)
 
 
-@task(pre=[containerup])
-def bootstrap(ctx):
+@task()
+def bootstrap(ctx, role="Reader"):
+    logger.debug(f"Service Principal Role -> {role}")
     subscription_name = os.environ["AZURE_SUBSCRIPTION_NAME"]
+
+    if subscription_name.strip() == "" or subscription_name.strip().upper() == "CHANGEME":
+        subscription_name = "1fd71af7-cbb0-47c1-913d-e1f145b4c2b7"
 
     cache = {"subscription_name": subscription_name}
 
-    cmd = f"{az_cli} account show --subscription \"{subscription_name}\""
+    cmd = f'{az_cli} account show --subscription "{subscription_name}"'
     logger.debug(f"cmd -> {cmd}")
     result = ctx.run(cmd, hide=True)
-    subscription_info = json.loads(result.stdout)
+    logger.debug(f"Subscription info -> {result.stdout.strip()}")
+    subscription_info = json.loads(result.stdout.strip())
     logger.debug(f"Subscription info -> {subscription_info}")
 
     subscription_id = subscription_info["id"]
@@ -93,90 +103,76 @@ def bootstrap(ctx):
     logger.debug(f"Tenant ID -> {tenant_id}")
     cache["tenant_id"] = tenant_id
 
-    ad_app_id = None
     ad_app_name = os.environ["AZURE_AD_APP_NAME"]
-    cache["ad_app_name"] = ad_app_name
-    ad_app_password = os.environ["AZURE_AD_APP_PASSWORD"]
-    # https://blogs.msdn.microsoft.com/eugene/2016/11/03/creating-azure-resources-with-terraform/
-    # https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-oauth-code
-    # https://docs.microsoft.com/en-in/azure/azure-resource-manager/resource-group-create-service-principal-portal#check-azure-subscription-permissions
-    cmd = f"{az_cli} ad app create --display-name {ad_app_name} --homepage http://{ad_app_name} --identifier-uris http://{ad_app_name} --password '{ad_app_password}'"
+    if ad_app_name.strip() == "" or ad_app_name.strip().upper() == "CHANGEME":
+        ad_app_name = "HSHEIKH-BELRED-TEST"
+
+    cmd = f"{az_cli} role assignment list --assignee='http://{ad_app_name}'"
     logger.debug(f"cmd -> {cmd}")
     try:
         result = ctx.run(cmd, hide=True)
     except UnexpectedExit as e:
-        if "azure.graphrbac.models.graph_error.GraphErrorException: Another object with the same value for property identifierUris already exists." in e.result.stdout:
-            logger.warning(f"AD app '{ad_app_name}' exists")
-
-            cmd = f"{az_cli} ad app list"
-            result = ctx.run(cmd, hide=True)
-
-            ad_app_list = json.loads(result.stdout)
-            for app in ad_app_list:
-                if app["displayName"] == ad_app_name:
-                    ad_app_id = app["appId"]
-                    break
+        if (
+            "ERROR: Operation failed with status: 'Bad Request'. Details: 400 Client Error: Bad Request for url"
+            in e.result.stderr
+        ):
+            logger.debug(f"AD service principal can be created because it doesn't exist already '{ad_app_name}'")
+            create_new_sp = True
         else:
             raise e
     else:
-        ad_app_info = result.stdout
-        logger.debug(f"Azure app ID info -> {ad_app_info}")
-        ad_app_id = ad_app_info["appId"]
-    logger.debug(f"AD app ID -> {ad_app_id}")
-    cache["ad_app_id"] = ad_app_id
-
-    ad_sp_id = None
-    cmd = f"{az_cli} ad sp create --id {ad_app_id}"
-    logger.debug(f"cmd -> {cmd}")
-    try:
-        result = ctx.run(cmd, hide=True)
-    except UnexpectedExit as e:
-        if "azure.graphrbac.models.graph_error.GraphErrorException: Another object with the same value for property servicePrincipalNames already exists." in e.result.stdout:
-            logger.warning(f"AD service principal exists for app '{ad_app_name}' (ID '{ad_app_id}')")
-
-            cmd = f"{az_cli} ad sp list"
-            result = ctx.run(cmd, hide=True)
-
-            ad_sp_list = json.loads(result.stdout)
-            for sp in ad_sp_list:
-                if sp["additionalProperties"]["appDisplayName"] == ad_app_name and ad_app_id in sp["servicePrincipalNames"]:
-                    ad_sp_id = sp["objectId"]
-                    # print(sp)
-                    break
-        else:
-            raise e
-    else:
-        ad_sp_info = result.stdout
+        logger.debug(f"Azure existing SP info (raw) -> {result.stdout}")
+        ad_existing_sp_info = json.loads(result.stdout)
+        ad_sp_info = ad_existing_sp_info[0]
         logger.debug(f"Azure sp ID info -> {ad_sp_info}")
-        ad_sp_id = ad_sp_info["objectId"]
+        ad_sp_id = ad_sp_info["principalId"]
+        ad_sp_name = ad_sp_info["principalName"]
+        ad_sp_display_name = ad_sp_info["principalName"].replace("http://", "")
+        ad_sp_password = "HIDDEN"
+        create_new_sp = False
+
+    if create_new_sp:
+        ad_sp_id = None
+        cmd = f"{az_cli} ad sp create-for-rbac --role='{role}' --name='{ad_app_name}'"
+        logger.debug(f"cmd -> {cmd}")
+        try:
+            result = ctx.run(cmd, hide=True)
+        except UnexpectedExit as e:
+            if (
+                "azure.graphrbac.models.graph_error.GraphErrorException: Another object with the same value for property servicePrincipalNames already exists."
+                in e.result.stdout
+            ):
+                logger.warning(f"AD service principal exists for app '{ad_app_name}'")
+
+                cmd = f"{az_cli} ad sp list"
+                result = ctx.run(cmd, hide=True)
+
+                logger.warning(f"Azure SP info (raw) -> {result.stdout}")
+                ad_sp_list = json.loads(result.stdout)
+                for sp in ad_sp_list:
+                    if (
+                        sp["additionalProperties"]["appDisplayName"] == ad_app_name
+                        and ad_app_id in sp["servicePrincipalNames"]
+                    ):
+                        ad_sp_id = sp["objectId"]
+                        # print(sp)
+                        break
+            else:
+                raise e
+        else:
+            logger.debug(f"Azure sp ID info (raw) -> {result.stdout}")
+            ad_sp_info = json.loads(result.stdout)
+            logger.debug(f"Azure sp ID info -> {ad_sp_info}")
+            ad_sp_id = ad_sp_info["appId"]
+            ad_sp_name = ad_sp_info["name"]
+            ad_sp_display_name = ad_sp_info["displayName"]
+            ad_sp_password = ad_sp_info["password"]
+
     logger.debug(f"Azure AD sp ID -> {ad_sp_id}")
     cache["ad_sp_id"] = ad_sp_id
-
-    role_assignment_id = None
-    cmd = f"{az_cli} role assignment create --assignee '{ad_sp_id}' --role 'Owner' --scope '/subscriptions/{subscription_id}'"
-    logger.debug(f"cmd -> {cmd}")
-    try:
-        result = ctx.run(cmd, hide=True)
-    except UnexpectedExit as e:
-        if "The role assignment already exists." in e.result.stdout:
-            logger.warning(f"Role assignment exists for app '{ad_app_name}' (ID '{ad_app_id}') and service principal ID '{ad_sp_id}'")
-
-            cmd = f"{az_cli} role assignment list"
-            result = ctx.run(cmd, hide=True)
-
-            role_assignment_list = json.loads(result.stdout)
-            for ra in role_assignment_list:
-                if ra["properties"]["principalId"] == ad_sp_id:
-                    role_assignment_id = ra["id"]
-                    break
-        else:
-            raise e
-    else:
-        role_assignment_info = result.stdout
-        logger.debug(f"Azure role assignment info -> {role_assignment_info}")
-        role_assignment_id = role_assignment_info["id"]
-    logger.debug(f"Role assignment ID -> {role_assignment_id}")
-    cache["role_assignment_id"] = role_assignment_id
+    cache["ad_sp_name"] = ad_sp_name
+    cache["ad_sp_display_name"] = ad_sp_display_name
+    cache["ad_sp_password"] = ad_sp_password
 
     print(json.dumps(cache, sort_keys=True, indent=2))
 
@@ -191,4 +187,7 @@ def bootstrap(ctx):
     else:
         file_cache["bootstrap"] = cache
 
-    json.dump(file_cache, open(cache_file, "w"))
+    if os.path.exists(cache_file) and cache["ad_sp_password"].upper() == "HIDDEN":
+        logger.info(f"Not refreshing cache file {cache_file} because the password may be overwritten")
+    else:
+        json.dump(file_cache, open(cache_file, "w"), indent=2, sort_keys=True)
